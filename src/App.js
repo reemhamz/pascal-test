34f7eb14ece96bdf7b1691699ba78fc81d3025a3
Reem H <reemhamoui92@gmail.com>
2020-03-12 17:26:10 -0400

    set up API call and working on functions to display images

--- src/App.js
+++ src/App.js
@@ -7,29 +7,63 @@ class App extends Component {
     super();
     this.state = {
       searchQuery: "",
-      collection: [],
+      collection: "",
+      collectData: ""
+      
     }
   }
 
-
+// Life cycle that includes the API call
   componentDidMount() {
-    const APIKey = "oXWNN3H3eBxih1adiK0VE1G9BG9XhdURrosI-WlOpzk";
 
-    axios({
-      method: 'GET',
-      // url: 
-
-    })
+    const searchCall = (searchData) => {
+      const apiKey = "oXWNN3H3eBxih1adiK0VE1G9BG9XhdURrosI-WlOpzk";
+      axios({
+        method: "GET",
+        url: `https://api.unsplash.com/search/photos?page=1&query=${this.state.searchQuery}&client_id=${apiKey}`,
+        dataResponse: "json"
+      }).then((response) => {
+        const responseObjects = response.data.results;
+        console.log(responseObjects)
+      })
+    }
+        this.setState({
+          collectData: searchCall
+        })
+    
   }
 
-    render() {
+
+  render() {
+      
+     // function for updating the search query
+    const queryChange = (e) => {
+      e.preventDefault();
+      this.setState({
+        searchQuery: e.target.value
+      })
+      }
+
+     // function for updating the collections drop down menu
+    const selectChange = (e) => { 
+        
+      }
+
+     // function for hitting submit
+      const querySubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.searchQuery)
+        return this.state.collectData();
+      }
+    
+    
         return (
           <div className="App">
             <div className="search">
           <h1>Image <span>search</span></h1>
-            <form action="" class="wrapper">
-              <input type="text" name="query" placeholder="Query" />
-                <select name="collections" id="collections">
+            <form action="" className="wrapper">
+                <input type="text" name="query" placeholder="Query" onChange={queryChange}/>
+                <select name="collections" id="collections" onChange={selectChange}>
                 <option value="wallpapers">Wallpapers</option>
                 <option value="nature">Nature</option>
                 <option value="film">Film</option>
@@ -38,7 +72,7 @@ class App extends Component {
                 <option value="people">People</option>
                 <option value="animals">Animals</option>
               </select>
-              <button type="submit">Search</button>
+                <button type="submit" onClick={querySubmit}>Search</button>
                   </form>
             </div>
             
