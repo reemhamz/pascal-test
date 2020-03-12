2d8cd64ff1434905eb78efa937221e095656831a
Reem H <reemhamoui92@gmail.com>
2020-03-11 20:37:07 -0400

    installed react and created lifecycles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchQuery: "",
+      collection: [],
+    }
+  }
+
+
+  componentDidMount() {
+    const APIKey = "oXWNN3H3eBxih1adiK0VE1G9BG9XhdURrosI-WlOpzk";
+
+    axios({
+      method: 'GET',
+      // url: 
+
+    })
+  }
+
+    render() {
+        return (
+            <div className="App">
+            <form action="">
+              <input type="text" name="query" placeholder="Query" />
+              <select name="collections" id="collections">
+                <option value="wallpapers">Wallpapers</option>
+                <option value="nature">Nature</option>
+                <option value="film">Film</option>
+                <option value="fashion">Fashion</option>
+                <option value="foodndrink">Food & Drink</option>
+                <option value="people">People</option>
+                <option value="animals">Animals</option>
+
+              </select>
+                  </form>
+            </div>
+        );
+    }
 }
 
 export default App;
